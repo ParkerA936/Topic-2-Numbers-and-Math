dc61e5c74927e774d70237b55e84b3ceb5341917
Parker Arnett <362581936@GoTVDSB.ca>
2026-02-05 12:19:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Topic_2_Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Parker
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(2026-17);
+            Console.WriteLine((14 + 15 + 16)/3);
+            Console.WriteLine((14.0 + 15.0 + 16.0+ 17.0) / 4);
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
