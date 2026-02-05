7f2cf7646dd28b45637414e676bbfdb84eb5b955
Parker Arnett <362581936@GoTVDSB.ca>
2026-02-05 12:53:28 -0500

    All Done

--- Program.cs
+++ Program.cs
@@ -1,21 +1,24 @@
-﻿namespace Topic_2_Numbers_and_Math
+﻿using System;
+
+namespace Topic_2_Numbers_and_Math
 {
     internal class Program
     {
         static void Main(string[] args)
         {
            //Parker
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine(2026-17);
-            Console.WriteLine((14 + 15 + 16)/3);
-            Console.WriteLine((14.0 + 15.0 + 16.0+ 17.0) / 4);
+            Console.WriteLine(2026 - 17);
             Console.WriteLine();
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine();
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine();
+            Console.WriteLine(3.14*9);
+            Console.WriteLine();
+            Console.WriteLine("C# knows when to add if the plus sign is next to two numbers but if it is next to a string it will concatenate. ");
 
 
-
-
-
-
+            
 
 
 
